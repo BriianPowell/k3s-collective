c85e87f72852cce2507c1c033494b7fc1ec05237
Brian Powell <brian@powell.place>
2023-03-10 12:24:51 -0800

    some Updates Across monitoring and Wiki.JS Normalization

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+const general = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  quoteProps: 'as-needed', // prettier default
+  bracketSpacing: true, // prettier default
+  bracketSameLine: false, // prettier default
+  arrowParens: 'always', // prettier default
+  proseWrap: 'preserve', // prettier default
+  singleAttributePerLine: false, // prettier default
+  embeddedLanguageFormatting: 'auto', // prettier default
+  // printWidth: // configured in .editorconfig->max_line_length
+  // useTabs: // configured in .editorconfig->indent_size
+  // tabWidth: // configured in .editorconfig->indent_style
+  // endOfLine: // configured in .editorconfig->end_of_line
+};
+
+module.exports = {
+  ...general,
+};
